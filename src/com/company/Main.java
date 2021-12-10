af9f6b1bba65e2860d10a1142593566609a42153
irendon1993 <isaacrendon7@gmail.com>
2021-12-10 17:11:37 -0500

    Initial commit/ Completed Challenge

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        short pounds = 176;
+        double kilos = (double)(pounds * 0.45359237);
+
+        System.out.println("If i weigh " + pounds + "lbs, then I weigh " + kilos + " in kilograms");
+    }
+}
